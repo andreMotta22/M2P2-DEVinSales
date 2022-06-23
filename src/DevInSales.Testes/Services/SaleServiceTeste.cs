c0cfc59497470930bab8ef24d61de58e5a4f6f72
andreMotta22 <andre_l_motta@estudante.sesisenai.org.br>
2022-06-23 17:28:22 -0300

    Feat: Finalizando o teste em StateService

---
+++ src/DevInSales.Testes/Services/SaleServiceTeste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevInSales.Testes.Services
+{
+    public class SaleServiceTeste
+    {
+
+        public Seeds() { }
+    }
+    
+}
